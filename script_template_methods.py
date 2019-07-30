232fbab529d7883d0eec766290e08fa5e8faacba
Brent Allen <carvetighter@gmail.com>
2019-07-29 22:51:13 -0500

    simplified files

--- script_template_methods.py
+++ script_template_methods.py
@@ -2,33 +2,24 @@
 methods file for sypder etc...
 '''
 
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-#
-# File / Package Import
-#
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-
-import warnings
+'''
+File / Package Import
+'''
 
 # supress warnings
+import warnings
 warnings.simplefilter('ignore')
 
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-#
-# Methods
-#
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+'''
+Methods
+'''
 
 def def_Methods(list_cluster_results, array_sparse_matrix):
     '''
     below is an example of a good method comment
-
+    
     -------------------------------------------------------------------------------------------#
-
+    
     this method implements the evauluation criterea for the clusters of each clutering algorithms
     criterea:
             - 1/2 of the clusters for each result need to be:
@@ -37,94 +28,76 @@ def def_Methods(list_cluster_results, array_sparse_matrix):
                 - the standard deviation of the clusters need to be lower than the standard deviation of all the clusters
                   combined
             - silhouette value for the dataset must be greater than 0.5
-
+    
     Requirements:
     package time
     package numpy
     package statistics
     package sklearn.metrics
-
+    
     Inputs:
     list_cluster_results
     Type: list
     Desc: the list of parameters for the clustering object
     list[x][0] -> type: array; of cluster results by sample in the order of the sample row passed as indicated by the sparse
-                   or dense array
+                    or dense array
     list[x][1] -> type: string; the cluster ID with the parameters
-
+    
     array_sparse_matrix
     Type: numpy array
     Desc: a sparse matrix of the samples used for clustering
-
+    
     Important Info:
     None
-
+    
     Return:
     object
     Type: list
     Desc: this of the clusters that meet the evaluation criterea
     list[x][0] -> type: array; of cluster results by sample in the order of the sample row passed as indicated by the sparse
-                   or dense array
+                    or dense array
     list[x][1] -> type: string; the cluster ID with the parameters
     list[x][2] -> type: float; silhouette average value for the entire set of data
     list[x][3] -> type: array; 1 dimensional array of silhouette values for each data sample
     list[x][4] -> type: list; list of lists, the cluster and the average silhoutte value for each cluster, the orders is sorted
-                       highest to lowest silhoutte value
-                       list[x][4][x][0] -> int; cluster label
-                       list[x][4][x][1] -> float; cluster silhoutte value
+                        highest to lowest silhoutte value
+                        list[x][4][x][0] -> int; cluster label
+                        list[x][4][x][1] -> float; cluster silhoutte value
     list[x][5] -> type: list; a list that contains the cluster label and the number of samples in each cluster
-                       list[x][5][x][0] -> int; cluster label
-                       list[x][5][x][1] -> int; number of samples in cluster list[x][5][x][0]
-   '''
-
-    #---------------------------------------------------------------------------------------------#
-    # objects declarations
-    #---------------------------------------------------------------------------------------------#
-
-    #---------------------------------------------------------------------------------------------#
-    # time declarations
-    #---------------------------------------------------------------------------------------------#
-
-    #---------------------------------------------------------------------------------------------#
-    # iteration declarations (list, set, tuple, counter, dictionary)
-    #---------------------------------------------------------------------------------------------#
-
-    list_return = list()
-
-    #---------------------------------------------------------------------------------------------#
-    # variables declarations
-    #---------------------------------------------------------------------------------------------#
-
-    #---------------------------------------------------------------------------------------------#
-    # db connections
-    #---------------------------------------------------------------------------------------------#
-
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #
-    # Start
-    #
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-
-    #---------------------------------------------------------------------------------------------#
-    # sub-section comment
-    #---------------------------------------------------------------------------------------------#
-
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #
-    # sectional comment
-    #
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-
-    #---------------------------------------------------------------------------------------------#
-    # variable / object cleanup
-    #---------------------------------------------------------------------------------------------#
-
-    #---------------------------------------------------------------------------------------------#
-    # return value
-    #---------------------------------------------------------------------------------------------#
-
-    return list_return
+                        list[x][5][x][0] -> int; cluster label
+                        list[x][5][x][1] -> int; number of samples in cluster list[x][5][x][0]
+    '''
+    
+    '''
+    objects declarations
+    '''
+    
+    '''
+    sequence declarations (list, set, tuple, counter, dictionary)
+    '''
+    
+    '''
+    variables declarations
+    '''
+    
+    '''
+    Start
+    '''
+    
+    '''
+    sub-section comment
+    '''
+    
+    '''
+    sectional comment
+    '''
+    
+    '''
+    variable / object cleanup
+    '''
+    
+    '''
+    return value
+    '''
+    
+    return
